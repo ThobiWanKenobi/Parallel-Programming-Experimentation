6d55b52012ee0671bec66a9d156b857c7aa0fbc2
ThobiWanKenobi <Thobias.Larsen@proton.me>
2026-09-05 15:47:19 +0200

    finish CUDA vector addition implementation, adding acceleration speedup and better validation

--- cuda_vector_addition/wa1-task3.cu
+++ cuda_vector_addition/wa1-task3.cu
@@ -7,13 +7,20 @@
 
 #include "helper.h"
 
-#define GPU_RUNS 100
+#define GPU_RUNS 300
+#define EPSILON 0.000001
 
 __global__ void addKernel(float* A, float *B, float *C, int length) {
     const unsigned int gid = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (gid < length) {
         C[gid] = A[gid] + B[gid];
+    }
+}
+
+void add(float *A, float *B, float *C, int length) {
+    for (int i = 0; i < length; i++) {
+        C[i] = A[i] + B[i];
     }
 }
 
@@ -45,6 +52,7 @@ int main(int argc, char** argv) {
     float* ha_in  = (float*) malloc(mem_size);
     float* hb_in  = (float*) malloc(mem_size);
     float* hc_out = (float*) malloc(mem_size);
+    float* hd_out = (float*) malloc(mem_size);
 
     // initialize the memory
     for(unsigned int i=0; i<N; ++i) {
@@ -72,12 +80,14 @@ int main(int argc, char** argv) {
     for(int r = 0; r < 1; r++) {
         addKernel<<<blocks, threads>>>(da_in, db_in, dc_out, N);
     }
+
+    double elapsed_gpu, elapsed_cpu;
   
     { // execute the kernel a number of times;
       // to measure performance use a large N, e.g., 200000000,
       // and increase GPU_RUNS to 100 or more. 
     
-        double elapsed; struct timeval t_start, t_end, t_diff;
+        struct timeval t_start, t_end, t_diff;
         gettimeofday(&t_start, NULL);
 
         for(int r = 0; r < GPU_RUNS; r++) {
@@ -101,11 +111,28 @@ int main(int argc, char** argv) {
         
         gettimeofday(&t_end, NULL);
         timeval_subtract(&t_diff, &t_end, &t_start);
-        elapsed = (1.0 * (t_diff.tv_sec*1e6+t_diff.tv_usec)) / GPU_RUNS;
-        double gigabytespersec = (3.0 * N * 4.0) / (elapsed * 1000.0);
-        printf("The kernel took on average %f microseconds. GB/sec: %f \n", elapsed, gigabytespersec);
+        elapsed_gpu = (1.0 * (t_diff.tv_sec*1e6+t_diff.tv_usec)) / GPU_RUNS;
+        double gigabytespersec = (3.0 * N * 4.0) / (elapsed_gpu * 1000.0);
+        printf("The kernel took on average %f microseconds. GB/sec: %f \n", elapsed_gpu, gigabytespersec);
         
     }
+
+    {
+        struct timeval t_start, t_end, t_diff;
+        gettimeofday(&t_start, NULL);
+        
+        add(ha_in, hb_in, hd_out, N);
+        
+        gettimeofday(&t_end, NULL);
+        timeval_subtract(&t_diff, &t_end, &t_start);
+        elapsed_cpu = (1.0 * (t_diff.tv_sec*1e6+t_diff.tv_usec));
+        double gigabytespersec = (3.0 * N * 4.0) / (elapsed_cpu * 1000.0);
+        printf("The CPU took on average %f microseconds. GB/sec: %f \n", elapsed_cpu, gigabytespersec);
+    }
+
+    double acceleration_speedup = elapsed_cpu / elapsed_gpu;
+
+    printf("The acceleration speedup is %f.\n", acceleration_speedup);
         
     // check for errors
     gpuAssert( cudaPeekAtLastError() );
@@ -118,8 +145,8 @@ int main(int argc, char** argv) {
 
     for(unsigned int i=0; i<N; ++i) {
         float actual   = hc_out[i];
-        float expected = ha_in[i] + hb_in[i]; 
-        if( actual != expected ) {
+        float expected = hd_out[i]; 
+        if(!(fabs(actual - expected) < EPSILON)) {
             printf("Invalid result at index %d, actual: %f, expected: %f. \n", i, actual, expected);
             exit(3);
         }
@@ -130,6 +157,7 @@ int main(int argc, char** argv) {
     free(ha_in);
     free(hb_in);
     free(hc_out);
+    free(hd_out);
     cudaFree(da_in);
     cudaFree(db_in);
     cudaFree(dc_out);
