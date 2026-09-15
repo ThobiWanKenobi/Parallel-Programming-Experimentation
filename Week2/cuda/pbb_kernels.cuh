b1d0ba1f4cfb7cacdb2a2a5c3eccc8a6e54c8d22
Thobias Larsen <Thobias.Larsen@proton.me>
2026-09-15 10:40:12 +0200

    Fix race condition bug possibly in CUDA implementation

--- Week2/cuda/pbb_kernels.cuh
+++ Week2/cuda/pbb_kernels.cuh
@@ -242,6 +242,7 @@ scanIncBlock(volatile typename OP::RedElTp* ptr, const uint32_t idx) {
     if (warpid > 0) {
         res = OP::apply(ptr[warpid-1], res);
     }
+    __syncthreads();
 
     return res;
 }
