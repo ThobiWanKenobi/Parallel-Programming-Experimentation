bf6dca2686a4ed7c2895ba377dc98c9669569272
Thobias Larsen <Thobias.Larsen@proton.me>
2026-09-20 11:15:29 +0200

    Cleanup

--- Week2/cuda/pbb_kernels.cuh
+++ Week2/cuda/pbb_kernels.cuh
@@ -244,7 +244,6 @@ scanIncBlock(volatile typename OP::RedElTp* ptr, const uint32_t idx) {
     if (warpid > 0) {
         res = OP::apply(ptr[warpid-1], res);
     }
-    __syncthreads(); // Fix to task 4
 
     return res;
 }
